2c8822acc33ab3269731d88524af078281b52d81
pratikMane07 <pratiksmane07@gmail.com>
2024-05-31 18:04:16 +0530

    add employee crud

---
+++ employeemcrud/src/main/java/com/app/cjc/EmployeemcrudApplication.java
@@ -0,0 +1,13 @@
+package com.app.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemcrudApplication.class, args);
+	}
+
+}
---
+++ employeemcrud/src/test/java/com/app/cjc/EmployeemcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.cjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
